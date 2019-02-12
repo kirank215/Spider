2dd826c5c5d7edc8f5ce389d26097631de2f6156
leo.naves <leo.naves@epita.fr>
2019-02-12 20:38:57 +0100

    listener: not compiling, working on constructor

--- src/events/listener.cc
+++ src/events/listener.cc
@@ -1,22 +1,30 @@
 #include "listener.hh"
 
+#include <ev.h>
+#include <iostream>
+
+#include "events/register.hh"
 #include "misc/socket.hh"
+#include "socket/socket.hh"
 
 http::ListenerEW::ListenerEW(shared_socket socket)
-    : sock_(socket)
+    : EventWatcher(socket->fd_get(), EV_READ)
 {
-    struct sockaddr_in* addr;
-    if (getsockname(socket.fd_get(), addr, sizeof(addr)) == -1)
+    sock_(socket) struct sockaddr_in* addr;
+    socklen_t len = sizeof(addr);
+    // TO FIX : Remove cast
+    if (getsockname(socket->fd_get()->fd_, (struct sockaddr*)addr, &len) == -1)
         std::cerr
             << "Listener constructor: trying to intialize with bad socket";
-    port_ = addr.sin_port;
+    port_ = addr->sin_port;
 }
 
 void http::ListenerEW::operator()()
 {
-    struct sockaddr_in addr*;
-    auto new_socket = accept(addr,sizeof(addr);
+    struct sockaddr* addr;
+    socklen_t len = sizeof(addr);
+    auto new_socket = sock_->accept(addr, &len);
 
-    event_register.register_ew();
+    http::event_register.register_ew(new_socket);
     // Add to loop
 }
