7592baf4a4d461f84f63fb84bcbd7593fa1e447a
leo.naves <leo.naves@epita.fr>
2019-02-14 15:43:26 +0100

    listener: written, needs to be tested, can't compile. Needs to be linked with eventwatcher classes constructors.

--- src/events/listener.cc
+++ src/events/listener.cc
@@ -2,29 +2,31 @@
 
 #include <ev.h>
 #include <iostream>
+#include <netinet/in.h>
 
 #include "events/register.hh"
 #include "misc/socket.hh"
 #include "socket/socket.hh"
 
 http::ListenerEW::ListenerEW(shared_socket socket)
-    : EventWatcher(socket->fd_get(), EV_READ)
+    : EventWatcher(socket->fd_get()->fd_, EV_READ)
 {
-    sock_(socket) struct sockaddr_in* addr;
+    sock_ = socket;
+    struct sockaddr* addr = new sockaddr();
     socklen_t len = sizeof(addr);
-    // TO FIX : Remove cast
-    if (getsockname(socket->fd_get()->fd_, (struct sockaddr*)addr, &len) == -1)
+    if (getsockname(socket->fd_get()->fd_, addr, &len) == -1)
         std::cerr
             << "Listener constructor: trying to intialize with bad socket";
-    port_ = addr->sin_port;
+    struct sockaddr_in* addr_in = reinterpret_cast<sockaddr_in*>(addr);
+    std::cout << addr_in->sin_port;
 }
 
 void http::ListenerEW::operator()()
 {
-    struct sockaddr* addr;
+    sockaddr addr;
     socklen_t len = sizeof(addr);
-    auto new_socket = sock_->accept(addr, &len);
+    auto new_socket = sock_->accept(&addr, &len);
 
-    http::event_register.register_ew(new_socket);
+    // http::event_register.register_ew(new_socket);
     // Add to loop
 }
