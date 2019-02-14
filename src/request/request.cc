5c80b0f2fcd806442b3d18ee7b287b593f5e2d4e
Mithun Murugesan <mithun.murugesan@epita.fr>
2019-02-14 20:27:14 +0100

    request : added constructor without handling sockets

---
+++ src/request/request.cc
@@ -0,0 +1,78 @@
+#include "request.hh"
+
+namespace http
+{
+    static method method_type(std::string str)
+    {
+        if(str.compare("GET") == 0)
+            return GET;
+        else if(str.compare("POST") == 0)
+            return POST;
+        else if(str.compare("HEAD") == 0)
+            return HEAD;
+        else
+            throw std::logic_error("unknown method_type");
+    }
+
+    static std::string get_token(int& pos, std::string delim,
+                                    const std::string buffer)
+    {
+        int count = 0;
+        auto found = buffer.find(delim, pos);
+        if(found != std::string::npos)
+        {
+            count = found - pos;
+            std::string res = buffer.substr(pos, count);
+            pos = found + 1;
+            return res;
+        }
+        else
+            throw std::logic_error("Parsing error in Request");
+    }
+
+    Request::Request(const std::string buffer)
+    {
+        /*  std::string buffer (4096, 0); //Ususally buffer lenght is 8kb
+            auto check = recv(new_socket, buffer, 4096, 0); // 0 is a flag
+            if (check < 0)
+            std::cout << "Error reading the request";
+            else
+            {*/
+        std::string type = "";
+        std::string body = "";
+        int i = 0;
+        int len = buffer.length();
+        m_ = method_type(get_token(i, " ", buffer));
+        request_uri_ = get_token(i, " ", buffer);
+        version_ = get_token(i, http_crlf, buffer);
+        msg_body_len_ = 0;
+        while(1)
+        {
+            i++;
+            type = get_token(i, ":", buffer);
+            body = get_token(i, http_crlf, buffer);
+            if(type.compare("Content-Length") == 0)
+            {
+                msg_body_len_ = stoi(body);
+            }
+            headers_.emplace(type, body);
+            if(i+2 < len)
+            {
+                if(buffer[i+1] == '\r' && buffer[i+2] == '\n')
+                {
+                    i = i+3;
+                    break;
+                }
+            }
+            else
+                break;
+        }
+        int max = i + msg_body_len_;
+        while(i <= max)
+        {
+            msg_body_.push_back(buffer[i]);
+            i++;
+        }
+        //}
+    }
+}
--- src/request/request.hh
+++ src/request/request.hh
@@ -5,7 +5,10 @@
 
 #pragma once
 
-#include "request/types.hh"
+#include "types.hh"
+#include "../misc/sys-wrapper.hh"
+#include <string>
+#include <algorithm>
 #include <map>
 namespace http
 {
@@ -17,16 +20,19 @@ namespace http
     struct Request
     {
         Request() = default;
+        Request(const std::string buffer); //Handle scoket initialization before
         Request(const Request&) = default;
         Request& operator=(const Request&) = default;
         Request(Request&&) = default;
         Request& operator=(Request&&) = default;
         ~Request() = default;
         // Add members to store the information relative to a request.
-        method m;
+        method m_;
         STATUS_CODE status;
-        std::string request_uri;
-        std::map<std::string, std::string> headers;
-        std::string msg_body;
+        std::string request_uri_;
+        std::string version_;
+        std::map<std::string, std::string> headers_;
+        int msg_body_len_;
+        std::string msg_body_;
     };
 } // namespace http
