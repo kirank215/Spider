159bbd7d3ed9b608651c5931923985b294ee731c
Mithun Murugesan <mithun.murugesan@epita.fr>
2019-02-15 20:33:37 +0100

    socket: debugging

--- src/socket/socket.cc
+++ src/socket/socket.cc
@@ -1,4 +1,5 @@
 #include "socket/socket.hh"
+#include <sys/socket.h>
 
 namespace http
 {
@@ -8,34 +9,34 @@ namespace http
 
     ssize_t Socket::recv(void* dst, size_t len)
     {
-        return recv(fd_, dest, len, 0);
+        return recv(fd_, dst, len, 0);
     }
-    ssize_t send(const void* src, size_t len)
+    ssize_t Socket::send(const void* src, size_t len)
     {
         return send(fd_, src, len, 0);
     }
-    ssize_t sendfile(misc::shared_fd& dst, off_t& off, size_t count)
+    ssize_t Socket::sendfile(misc::shared_fd& dst, off_t& off, size_t count)
     {
-        return sendfile(fd_, dst.fd_get()->fd_, off, count);
+        return sendfile(fd_, dst, off, count);
     }
-    void bind(const sockaddr* addr, socklen_t addrlen)
+    void Socket::bind(const sockaddr* addr, socklen_t addrlen)
     {
         bind(fd_, addr, addrlen);
     }
-    void listen(int backlog)
+    void Socket::listen(int backlog)
     {
         listen(fd_, backlog)
     }
-    void setsockopt(int level, int optname, int optval)
+    void Socket::setsockopt(int level, int optname, int optval)
     {
         setsockopt(fd_, level, optname, optval, sizeof(optval));
     }
-    std::shared_ptr<Socket> accept(sockaddr* addr,
+    std::shared_ptr<Socket> Socket::accept(sockaddr* addr,
             socklen_t* addrlen)
     {
         return accept(fd_, addr, addrlen);
     }
-    void connect(const sockaddr*, socklen_t addrlen)
+    void Socket::connect(const sockaddr*, socklen_t addrlen)
     {
         connect(fd_, sockaddr, addrlen);
     }
