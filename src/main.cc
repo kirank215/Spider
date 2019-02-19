f9345f8069365405f09256340418f6919229b958
Mithun Murugesan <mithun.murugesan@epita.fr>
2019-02-19 21:57:30 +0100

    main : frame updated:

--- src/main.cc
+++ src/main.cc
@@ -1,23 +1,27 @@
 #include "error/not-implemented.hh"
 #include "config/config.hh"
 #include <iostream>
-
+#include <cstring>
 using namespace http;
 
 int main(int argc, char *argv[])
 {
-    using namespace http;
-    int i = 1;
-    for(; i < argc; i++)
+    bool dry = 0;
+    if(argc >3 || argc <= 1)
+        throw std::logic_error("Invalid number of arguments");
+    if(strcmp(argv[1],"-t") == 0)
+        dry = 1;
+    ServerConfig lol;
+    try
     {
-        if(argv[i][0] != '-')
-            break;
+        lol = parse_configuration(argv[dry+1], dry);
     }
-    struct ServerConfig serveurconfig = http::parse_configuration(argv[i]);
-    return 1;
-    /*   TESTING PARSE CONFIG
-    const std::string path= "configure_file.json";
-    ServerConfig lol = parse_configuration(path);
+    catch(std::exception& e)
+    {
+        std::cerr << e.what();
+        exit(1);
+    }
+    /*TESTING PARSING*/
     for(auto it = lol.list_vhost.begin(); it != lol.list_vhost.end(); it++)
     {
         VHostConfig foo = *it;
@@ -27,6 +31,4 @@ int main(int argc, char *argv[])
         std::cout << "root : " << foo.root<< "\n";
         std::cout << "default_file : " << foo.default_file<< "\n\n";
     }
-    throw http::NotImplemented();
-*/
 }
