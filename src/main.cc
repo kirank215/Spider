01e9bbf3dbb265d28538cc47bcfcb7bc96552ab6
Salome Desauty <salome.desauty@epita.fr>
2019-02-19 19:14:16 +0100

    main : started the implementation of the main

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,15 @@
 #include "error/not-implemented.hh"
+#include "config/config.hh"
 
-int main()
+int main(int argc, char *argv[])
 {
-    throw http::NotImplemented();
+    using namespace http;
+    int i = 1;
+    for(; i < argc; i++)
+    {
+        if(argv[i][0] != '-')
+            break;
+    }
+    struct ServerConfig serveurconfig = http::parse_configuration(argv[i]);
+    return 1;
 }
