87172fa164f8587c22d8102620da63ac6eaf6741
Mithun Murugesan <mithun.murugesan@epita.fr>
2019-02-19 20:28:17 +0100

    main : created framework for json parse call

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,8 @@
 #include "error/not-implemented.hh"
 #include "config/config.hh"
+#include <iostream>
+
+using namespace http;
 
 int main(int argc, char *argv[])
 {
@@ -12,4 +15,18 @@ int main(int argc, char *argv[])
     }
     struct ServerConfig serveurconfig = http::parse_configuration(argv[i]);
     return 1;
+    /*   TESTING PARSE CONFIG
+    const std::string path= "configure_file.json";
+    ServerConfig lol = parse_configuration(path);
+    for(auto it = lol.list_vhost.begin(); it != lol.list_vhost.end(); it++)
+    {
+        VHostConfig foo = *it;
+        std::cout << "IP : " << foo.ip << "\n";
+        std::cout << "port : " << (unsigned)foo.port<< "\n";
+        std::cout << "server_name : " << foo.server_name<< "\n";
+        std::cout << "root : " << foo.root<< "\n";
+        std::cout << "default_file : " << foo.default_file<< "\n\n";
+    }
+    throw http::NotImplemented();
+*/
 }
