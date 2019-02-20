ad7e030b78c0f0eddbac79349dbe288017aa8782
leo.naves <leo.naves@epita.fr>
2019-02-20 19:22:33 +0100

    fd: fixed destructor

--- src/misc/fd.hh
+++ src/misc/fd.hh
@@ -27,7 +27,10 @@ namespace misc
             : fd_{fd}
         {}
 
-        ~FileDescriptor()=default; //FIXME changed to default
+        ~FileDescriptor()
+        {
+            close(fd_);
+        };
 
         FileDescriptor(const FileDescriptor&) = delete;
         FileDescriptor& operator=(const FileDescriptor&) = delete;
