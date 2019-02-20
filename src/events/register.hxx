f45e73eda66e651bbab5af7ac8a61e29e77e76de
leo.naves <leo.naves@epita.fr>
2019-02-20 20:28:52 +0100

    register: implemented remaining functions

--- src/events/register.hxx
+++ src/events/register.hxx
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <optional>
+
 #include "register.hh"
 
 namespace http
@@ -12,5 +14,23 @@ namespace http
         events_[ew.get()] = ew;
         loop_.register_watcher(ew.get());
         return ew;
+    }
+
+    bool EventWatcherRegistry::unregister_ew(EventWatcher* ew)
+    {
+        auto to_remove = events_.find(ew);
+        if (to_remove == events_.end())
+            return false;
+        events_.erase(to_remove);
+        return true;
+    }
+
+    std::optional<std::shared_ptr<EventWatcher>>
+    EventWatcherRegistry::at(EventWatcher* ew)
+    {
+        auto res = events_.find(ew);
+        if (res != events_.end())
+            return res->second;
+        return std::nullopt;
     }
 } // namespace http
