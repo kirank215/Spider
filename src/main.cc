422f664beb98dbdd20bee5ee05b6454b42be8168
Krishnaswamy Kiran Krishna <kirankrishna.krishnaswamy@epita.fr>
2019-05-01 21:26:14 +0200

    mutiple hosts without https

--- src/main.cc
+++ src/main.cc
@@ -51,60 +51,62 @@ int main(int argc, char *argv[])
     hints.socktype(SOCK_STREAM);
 
     //server socket creation and binding
-    VHostConfig vc = sc.list_vhost[0];
-    auto vstatic = VHostFactory::Create(vc);
-
-    dispatcher.insert_staticfile(vstatic);
-    AddrInfo addrinfo = misc::getaddrinfo(vc.ip.c_str(),
-            std::to_string(vc.port).c_str(), hints);
-    /*
-       for(auto &i : addrinfo)
-       {
-       if(i.ai_family == AF_INET)
-       {
-       struct sockaddr_in *ipv4 = (struct sockaddr_in *)i.ai_addr;
-       i.ai_addr->sockaddr = ipv4;
-       }
-       else
-       {
-       struct sockaddr_in *ipv6 = (struct sockaddr_in6 *)i.ai_addr;
-       i.ai_addr = ipv6;
-       }
-       }
-     */
-    std::shared_ptr<DefaultSocket> sha_sock;
-    if((*addrinfo.begin()).ai_family == AF_INET)
-        sha_sock = std::make_shared<DefaultSocket>
-            (AF_INET , SOCK_STREAM, 0);
-    else
+    for(VHostConfig vc : sc.list_vhost)
     {
-        sha_sock = std::make_shared<DefaultSocket>
-            (AF_INET6 , SOCK_STREAM, 0);
-        sha_sock->ipv6_set(true);
-    }
-    sha_sock->setsockopt(SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR), 1);
-    sys::fcntl_set((*(sha_sock)->fd_get()), O_NONBLOCK);
+        auto vstatic = VHostFactory::Create(vc);
 
-    for(auto &i : addrinfo)
-    {
-        try
+        dispatcher.insert_staticfile(vstatic);
+        AddrInfo addrinfo = misc::getaddrinfo(vc.ip.c_str(),
+                std::to_string(vc.port).c_str(), hints);
+        /*
+           for(auto &i : addrinfo)
+           {
+           if(i.ai_family == AF_INET)
+           {
+           struct sockaddr_in *ipv4 = (struct sockaddr_in *)i.ai_addr;
+           i.ai_addr->sockaddr = ipv4;
+           }
+           else
+           {
+           struct sockaddr_in *ipv6 = (struct sockaddr_in6 *)i.ai_addr;
+           i.ai_addr = ipv6;
+           }
+           }
+         */
+        std::shared_ptr<DefaultSocket> sha_sock;
+        if((*addrinfo.begin()).ai_family == AF_INET)
+            sha_sock = std::make_shared<DefaultSocket>
+                (AF_INET , SOCK_STREAM, 0);
+        else
         {
-            sha_sock->bind(i.ai_addr, i.ai_addrlen);
+            sha_sock = std::make_shared<DefaultSocket>
+                (AF_INET6 , SOCK_STREAM, 0);
+            sha_sock->ipv6_set(true);
         }
-        catch(std::system_error&)
+        sha_sock->setsockopt(SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR), 1);
+        sys::fcntl_set((*(sha_sock)->fd_get()), O_NONBLOCK);
+
+        for(auto &i : addrinfo)
         {
-            continue;
+            try
+            {
+                sha_sock->bind(i.ai_addr, i.ai_addrlen);
+            }
+            catch(std::system_error&)
+            {
+                continue;
+            }
         }
+        sha_sock->listen(10);
+
+        http::Request::header_max_size = 8000;  // @Salome add this VHostCofig
+        http::Request::uri_max_size = 2000;     // values here after finishing
+        http::Request::payload_max_size = 2000;  // that part FIXME
+
+        //calling listener
+        /*EventWatcherRegistry event_register();*/
+        event_register.register_ew<ListenerEW>(sha_sock);
     }
-    sha_sock->listen(10);
-
-    http::Request::header_max_size = 8000;  // @Salome add this VHostCofig 
-    http::Request::uri_max_size = 2000;     // values here after finishing
-    http::Request::payload_max_size = 2000;  // that part FIXME
-
-    //calling listener
-    /*EventWatcherRegistry event_register();*/
-    event_register.register_ew<ListenerEW>(sha_sock);
     event_register.loop_get()();
 
 
