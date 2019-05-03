979d000b04a74f96671b8c396665dd8699ec3673
Salome Desauty <salome.desauty@epita.fr>
2019-05-03 19:16:47 +0200

    fixed config files

--- src/config/config.cc
+++ src/config/config.cc
@@ -39,93 +39,89 @@ namespace http
     {
         json j;
         j = create_json(path);
+        json elt = j;
         struct ServerConfig SC;
         SC.default_vhost_found = false;
         if(!dry)
         {
             std::vector<VHostConfig> list_vhost;
-            for(auto elt : j)
+            for(unsigned i = 0; i < elt.size(); i++)
             {
-                for(unsigned int i = 0; i < elt.size(); i++)
+                struct VHostConfig v;
+                if(elt["vhosts"][i]["ip"].is_string() &&
+                        elt["vhosts"][i]["port"].is_number() &&
+                        elt["vhosts"][i]["server_name"].is_string())
                 {
-                    struct VHostConfig v;
-                    if(elt[i]["ip"].is_string() &&
-                            elt[i]["port"].is_number() &&
-                            elt[i]["server_name"].is_string())
+                    v.ip = elt["vhosts"][i]["ip"];
+                    v.server_name = elt["vhosts"][i]["server_name"];
+                    v.port = elt["vhosts"][i]["port"];
+                    if(elt["vhosts"][i]["proxy_pass"] != nullptr)
                     {
-                        v.ip = elt[i]["ip"];
-                        v.server_name = elt[i]["server_name"];
-                        v.port = elt[i]["port"];
-                        if(elt[i]["proxy_pass"])
+                        if ((elt["vhosts"][i]["proxy_pass"]["ip"].is_string()) &&
+                                (elt["vhosts"][i]["proxy_pass"]["port"].is_number()))
                         {
-                            if ((elt[i]["proxy_pass"]["ip"].is_string()) &&
-                                (elt[i]["proxy_pass"]["port"].is_number()))
-                                {
-                                    struct Proxy_pass proxy_pass;
-                                    proxy_pass.ip = elt[i]["proxy_pass"]["ip"];
-                                    proxy_pass.port = elt[i]["proxy_pass"]["port"];
+                            struct Proxy_pass proxy_pass;
+                            proxy_pass.ip = elt["vhosts"][i]["proxy_pass"]["ip"];
+                            proxy_pass.port = elt["vhosts"][i]["proxy_pass"]["port"];
 
-                                    if((elt[i]["proxy_pass"]["proxy_set_header"]))
-                                    {
-                                        proxy_pass.proxy_set_header =
-                                            elt[i]["proxy_pass"]["proxy_set_header"].get<std::map<std::string, std::string>>();
-                                    }
-                                    if((elt[i]["proxy_pass"]["proxy_remove_header"]))
-                                    {
-                                        for(int k = 0; elt[i]["proxy_pass"]["proxy_remove_header"][k]; k++)
-                                        {
-                                            proxy_pass.proxy_remove_header.push_back(elt[i]["proxy_pass"]["proxy_remove_header"][k]);
-                                        }
-                                    }
-                                    if((elt[i]["proxy_pass"]["set_header"]))
-                                    {
-                                        proxy_pass.set_header =
-                                            elt[i]["proxy_pass"]["set_header"].get<std::map<std::string, std::string>>();
-                                    }
-                                    if((elt[i]["proxy_pass"]["remove_header"]))
-                                    {
-                                        for(int k = 0; elt[i]["proxy_pass"]["remove_header"][k]; k++)
-                                        {
-                                            proxy_pass.remove_header.push_back(elt[i]["proxy_pass"]["remove_header"][k]);
-                                        }
-                                    }
-                                    v.proxy_pass = proxy_pass;
-                                }
-                        }
-                        else
-                        {
-                            v.root = elt[i]["root"];
-                            if(elt[i]["default_file"].is_string())
-                                v.default_file = elt[i]["default_file"];
-                            else
-                                v.default_file = "index.html";
-                        }
-                        if(elt[i]["ssl_cert"] && elt[i]["ssl_cert"].is_string()
-                                && elt[i]["ssl_key"] && elt[i]["ssl_key"].is_string())
-                        {
-                            v.ssl_cert = elt[i]["ssl_cert"];
-                            v.ssl_key = elt[i]["ssl_key"];
-                        }
-                        if(elt[i]["auth_basic_user"]
-                                && elt[i]["auth_basic"] && elt[i]["auth_basic"].is_string())
-                        {
-                            v.auth_basic = elt[i]["auth_basic"];
-                            //list of user for loop to add every strings...
-                        }
-                        if(elt[i]["health_endpoint"])
-                            v.health_endpoint = elt[i]["health_endpoint"];
-                        if(elt[i]["default_vhost"]) // equal to true
-                        {
-                            if(!SC.default_vhost_found)
+                            if((elt["vhosts"][i]["proxy_pass"]["proxy_set_header"]) != nullptr)
                             {
-                                SC.default_vhost_found = true;
-                                //need to set this vhost as the default one
+                                proxy_pass.proxy_set_header =
+                                    elt["vhosts"][i]["proxy_pass"]["proxy_set_header"].get<std::map<std::string, std::string>>();
                             }
+                            if((elt["vhosts"][i]["proxy_pass"]["proxy_remove_header"]) != nullptr)
+                            {
+                                for(auto a : elt["vhosts"][i]["proxy_pass"]["proxy_remove_header"])
+                                {
+                                    proxy_pass.proxy_remove_header.push_back(a);
+                                }
+                            }
+                            if((elt["vhosts"][i]["proxy_pass"]["set_header"]) != nullptr)
+                            {
+                                proxy_pass.set_header =
+                                    elt["vhosts"][i]["proxy_pass"]["set_header"].get<std::map<std::string, std::string>>();
+                            }
+                            if((elt["vhosts"][i]["proxy_pass"]["remove_header"]) != nullptr)
+                            {
+                                for(auto a : elt["vhosts"][i]["proxy_pass"]["remove_header"])
+                                {
+                                    proxy_pass.remove_header.push_back(a);
+                                }
+                            }
+                            v.proxy_pass = proxy_pass;
                         }
-                        list_vhost.push_back(v);
                     }
                     else
-                        throw std::logic_error("Vhost incomplete\n");
+                    {
+                        v.root = elt["vhosts"][i]["root"];
+                        if(elt["vhosts"][i]["default_file"].is_string())
+                            v.default_file = elt["vhosts"][i]["default_file"];
+                        else
+                            v.default_file = "index.html";
+                    }
+                    if(elt["vhosts"][i]["ssl_cert"] != nullptr && elt["vhosts"][i]["ssl_cert"].is_string()
+                            && elt["vhosts"][i]["ssl_key"]  != nullptr && elt["vhosts"][i]["ssl_key"].is_string())
+                    {
+                        v.ssl_cert = elt["vhosts"][i]["ssl_cert"];
+                        v.ssl_key = elt["vhosts"][i]["ssl_key"];
+                    }
+                    if(elt["vhosts"][i]["auth_basic_user"] != nullptr
+                            && elt["vhosts"][i]["auth_basic"] != nullptr && elt["vhosts"][i]["auth_basic"].is_string())
+                    {
+                        v.auth_basic = elt["vhosts"][i]["auth_basic"];
+                        //list of user for loop to add every strings...
+                    }
+                    if(elt["vhosts"][i]["health_endpoint"] != nullptr)
+                        v.health_endpoint = elt["vhosts"][i]["health_endpoint"];
+                    if(elt["vhosts"][i]["default_vhost"] != nullptr) // equal to true
+                    {
+                        if(!SC.default_vhost_found)
+                        {
+                            SC.default_vhost_found = true;
+                            //need to set this vhost as the default one
+                        }
+                    }
+                    list_vhost.push_back(v);
                 }
             }
             SC.list_vhost = list_vhost;
